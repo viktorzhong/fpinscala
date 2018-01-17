8e37c5eb159fec78be075d68b574f7b0ae39c67c
viktorzhong <vzhong81@gmail.com>
2018-01-17 21:03:29 +0800

    Add solution for 2.3 2.4.

--- src/main/scala/solutions/Chap2.scala
+++ src/main/scala/solutions/Chap2.scala
@@ -15,6 +15,7 @@ object Chap2 {
 
   /**
     * solution for exercise 2.2
+    * 思路: 比较相邻的两个元素是否有序并递归调用
     */
   def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
     @annotation.tailrec
@@ -27,5 +28,21 @@ object Chap2 {
     loop(0)
   }
 
+  /**
+    * solution for exercise 2.3
+    * 把f转换为只有一个参数的部分应用函数
+    */
+  def curry[A,B,C](f:(A, B) => C) : A => (B => C) = {
+      (f _).curried
+  }
+
+  /**
+    * solution for exercise 2.3
+    * 实现反科里化
+    * 思路: 调用Function.uncurried()
+    */
+  def uncurry[A, B, C](f: A => B => C) : (A, B) => C = {
+    Function.uncurried(f)
+  }
 
 }
