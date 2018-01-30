b32009d463b6464eeaa5fcc8070eb266e283d312
viktorzhong <vzhong81@gmail.com>
2018-01-30 23:55:25 +0800

    Add solution for 3.9

--- src/main/scala/solutions/Chap3.scala
+++ src/main/scala/solutions/Chap3.scala
@@ -1,6 +1,6 @@
 package solutions
 
-import List.sum
+import List._
 
 object Chap3 extends App {
 
@@ -14,5 +14,19 @@ object Chap3 extends App {
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
     case Cons(h, t) => h + sum(t)
     case _ => 101
+  }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = {
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+  }
+
+  /**
+    * 练习3.9 用foldRight实现List长度
+    */
+  def length[A](as: List[A]): Int = {
+    foldRight(as, 0)((_, t) => t + 1)
   }
 }
--- src/main/scala/solutions/List.scala
+++ src/main/scala/solutions/List.scala
@@ -76,4 +76,6 @@ object List {
     }
   }
 
+
+
 }
