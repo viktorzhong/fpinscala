4ecb9407ff1de9a7b05a1298b0263a6818ef4a5d
viktorzhong <vzhong81@gmail.com>
2018-02-04 11:53:11 +0800

    Add solution for 3.10

--- src/main/scala/solutions/List.scala
+++ src/main/scala/solutions/List.scala
@@ -78,11 +78,11 @@ object List {
 
   /**
     * 练习3.10 尾递归实现foldLeft
+    * 思路: 注意和foldRight的区别， foldLeft为左结合 foldRight是右结合
     */
   def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = {
     as match {
       case Nil => z
-      case Cons(h, Nil) => f(z, h)
       case Cons(h,xs) => foldLeft(xs, f(z, h))(f)
     }
   }
